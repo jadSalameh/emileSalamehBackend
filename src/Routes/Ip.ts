4aafc3be31749276e25c922929a3a295ee7e82ce
jadSalameh <jjadsalamehh@gmail.com>
2022-03-13 00:54:41 +0200

    for heroku test

--- src/Routes/Ip.ts
+++ src/Routes/Ip.ts
@@ -6,5 +6,8 @@ export function ipRoutes(fastify: FastifyInstance, options: any, done: any) {
   fastify.post("/ip", (req, res) => {
     let knex = req.requestContext.get("knex");
   });
+  fastify.get("/", async (req, res) => {
+    return "hello";
+  });
   done();
 }
